3c8d8a9a686c7f24dc46243948af21afdc18de21
lovesahaj-max <love.singh@maxhealthcare.com>
2024-11-17 01:20:07 +0530

    sorting with isort then black

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -425,10 +425,15 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'black', 'isort' },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+      formatters = {
+        black = {
+          prepend_args = { '--line-length', '80', '--fast' },
+        },
       },
     },
     init = function()
